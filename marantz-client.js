fd941feb5777b6394cf669f4aeeaff8b665c0057
Eric Pinzur <ericpinzur@otherinbox.com>
2017-02-23 00:54:33 -0600

    progess saving state and expanding to 2 zones

--- marantz-client.js
+++ marantz-client.js
@@ -1,9 +1,19 @@
 var telnet = require('telnet-client');
 var connection = new telnet();
 
-var params = {
-    host: 'marantz',
-    shellPrompt: ''
+var state = {
+    main: {
+        isPowered: false,
+        source: null,
+        mainLevel: null,
+        isMuted: false
+    },
+    aux: {
+        isPowered: false,
+        source: null,
+        mainLevel: null,
+        isMuted: false
+    }
 };
 
 connection.on('close', function() {
@@ -16,62 +26,196 @@ connection.on('error', function(error) {
 
 connection.on('data', function(data) {
     data = data.toString().trim();
+    switch (data) {
+        case 'SISAT/CBL':
+            state.main.source = 'AppleTV';
+            console.log('Main Source: AppleTV');
+            return;
+        case 'SIDVD':
+            state.main.source = 'ChromeCast';
+            console.log('Main Source: ChromeCast');
+            return;
+        case 'SIBD':
+            state.main.source = 'Roku';
+            console.log('Main Source: Roku');
+            return;
+        case 'Z2SAT/CBL':
+            state.aux.source = 'AppleTV';
+            console.log('Aux Source: AppleTV');
+            return;
+        case 'Z2DVD':
+            state.aux.source = 'ChromeCast';
+            console.log('Aux Source: ChromeCast');
+            return;
+        case 'Z2BD':
+            state.aux.source = 'Roku';
+            console.log('Aux Source: Roku');
+            return;
+        case 'Z2OFF':
+            state.aux.isPowered = false;
+            console.log('Aux Power: Off');
+            return;
+        case 'Z2ON':
+            state.aux.isPowered = true;
+            console.log('Aux Power: On');
+            return;
+        case 'ZMOFF':
+            state.main.isPowered = false;
+            console.log('Main Power: Off');
+            return;
+        case 'ZMON':
+            state.main.isPowered = true;
+            console.log('Main Power: On');
+            return;
+        case 'MUOFF':
+            state.main.isMuted = false;
+            console.log('Main Mute: Off');
+            return;
+        case 'MUON':
+            state.main.isMuted = true;
+            console.log('Main Mute: On');
+            return;
+        case 'Z2MUOFF':
+            state.aux.isMuted = false;
+            console.log('Aux Mute: Off');
+            return;
+        case 'Z2MUON':
+            state.aux.isMuted = true;
+            console.log('Aux Mute: On');
+            return;
+    }
     if (data.indexOf('MV') == 0) {
-        data = data.substr(2);
-        if  (data.indexOf('MAX') != 0) {
-            if (data.length == 3) {
-                data = data.substring(0,2);
-                data = parseFloat(data);
-                data += 0.5;
-            } else {
-                data = parseFloat(data);
-            }
-            console.log('Living Room Volume: ' + data);
+        var main_vol = getVolume(data);
+        if  (main_vol !== false) {
+            state.main.mainLevel = main_vol;
+            console.log('Main Volume: ' + main_vol);
             return;
         }
     }
-    switch (data) {
-        case 'SISAT/CBL':
-            console.log('Living Room Source: AppleTV');
+    if (data.indexOf('Z2') == 0) {
+        var aux_vol = getVolume(data);
+        if  (aux_vol !== false) {
+            state.aux.mainLevel = aux_vol;
+            console.log('Aux Volume: ' + aux_vol);
             return;
-        case 'SIDVD':
-            console.log('Living Room Source: ChromeCast');
-            return;
-        case 'SIBD':
-            console.log('Living Room Source: Roku');
-            return;
-
+        }
     }
+    console.log('\t\t\t\tUnknown data: ' +  data);
 });
 
+function getVolume(data) {
+    var vol = data.substr(2);
+    if  (/^[0-9]{3}$/.test(vol)) {
+        vol = vol.substring(0, 2);
+        vol = parseFloat(vol);
+        vol += 0.5;
+        return vol;
+    } else if (/^[0-9]{2}$/.test(vol)) {
+        return parseFloat(vol);
+    }
+    return false;
+}
 
-connection.connect(params);
+connection.connect({host: 'marantz', shellPrompt: ''});
 
-module.exports.volumeUp = function() {
-    connection.send('MVUP\r');
-    return true;
+setTimeout(function() { connection.send('SI?\r'); }, 1000);
+setTimeout(function() { connection.send('ZM?\r'); }, 2000);
+setTimeout(function() { connection.send('MV?\r'); }, 3000);
+setTimeout(function() { connection.send('MU?\r'); }, 4000);
+setTimeout(function() { connection.send('Z2?\r'); }, 5000);
+setTimeout(function() { connection.send('Z2MU?\r'); }, 6000);
+
+module.exports.getState = function() {
+    return state;
 };
 
-module.exports.volumeDown = function() {
-    connection.send('MVDOWN\r');
-    return true;
+module.exports.setPower = function(device, isOn) {
+    switch (device) {
+        case 'main':
+            if (isOn) {
+                connection.send('ZMON\r'); //might be PWON
+            } else {
+                connection.send('ZMOFF\r'); //might be PWSTANDBY
+            }
+            return true;
+        case 'aux':
+            if (isOn) {
+                connection.send('Z2ON\r');
+            } else {
+                connection.send('Z2OFF\r');
+            }
+            return true;
+    }
+    return false;
 };
 
-module.exports.volumeDirect = function(volume) {
-    connection.send('MV' + volume + '\r');
-    return true;
+module.exports.setMute = function(device, isOn) {
+    switch (device) {
+        case 'main':
+            if (isOn) {
+                connection.send('MUON\r');
+            } else {
+                connection.send('MUOFF\r');
+            }
+            return true;
+        case 'aux':
+            if (isOn) {
+                connection.send('Z2MUON\r');
+            } else {
+                connection.send('Z2MUOFF\r');
+            }
+            return true;
+    }
+    return false;
 };
 
-module.exports.source = function(source) {
+module.exports.volumeUp = function(device) {
+    switch (device) {
+        case 'main':
+            connection.send('MVUP\r');
+            return true;
+        case 'aux':
+            connection.send('Z2UP\r');
+            return true;
+    }
+    return false;
+};
+
+module.exports.volumeDown = function(device) {
+    switch (device) {
+        case 'main':
+            connection.send('MVDOWN\r');
+            return true;
+        case 'aux':
+            connection.send('Z2DOWN\r');
+            return true;
+    }
+    return false;
+};
+
+module.exports.setVolume = function(device, volume) {
+    switch (device) {
+        case 'main':
+            connection.send('MV' + volume + '\r');
+            return true;
+        case 'aux':
+            connection.send('Z2' + volume + '\r');
+            return true;
+    }
+    return false;
+};
+
+module.exports.setSource = function(device, source) {
+    var prefix = (device == 'main' ? 'SI' : 'Z2');
     switch (source) {
         case 'AppleTV':
-            connection.send('SISAT/CBL\r');
+            connection.send(prefix + 'SAT/CBL\r');
             break;
         case 'ChromeCast':
-            connection.send('SIDVD\r');
+            connection.send(prefix + 'DVD\r');
             break;
         case 'Roku':
-            connection.send('SIBD\r');
+            connection.send(prefix + 'BD\r');
             break;
         default:
             return false;
--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,35 @@ var express = require('express');
 var router = express.Router();
 var marantz = require('../marantz-client');
 
-router.post('/volume/up', function(req, res, next) {
-    marantz.volumeUp();
+router.get('/state', function(req, res, next) {
+    res.status(200).json(marantz.getState());
+});
+
+router.get('/sources', function(req, res, next) {
+    res.status(200).json([
+        'AppleTV',
+        'Roku',
+        'ChromeCast'
+    ]);
+});
+
+router.post('/zone/:zone/volume/up', function(req, res, next) {
+    marantz.volumeUp(req.params.zone);
     res.sendStatus(200);
 });
 
-router.post('/volume/down', function(req, res, next) {
-    marantz.volumeDown();
+router.post('/zone/:zone/volume/down', function(req, res, next) {
+    marantz.volumeDown(req.params.zone);
     res.sendStatus(200);
 });
 
-router.post('/volume/:level', function(req, res, next) {
-    marantz.volumeDirect(req.params.level);
+router.post('/zone/:zone/volume/:level', function(req, res, next) {
+    marantz.setVolume(req.params.zone, req.params.level);
     res.sendStatus(200);
 });
 
-router.post('/source/:source', function(req, res, next) {
-    if (marantz.source(req.params.source)) {
+router.post('/zone/:zone/source/:source', function(req, res, next) {
+    if (marantz.setSource(req.params.zone, req.params.source)) {
         res.sendStatus(200);
     } else {
         res.sendStatus(404);
