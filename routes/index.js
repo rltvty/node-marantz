d4998ef909effeaeb73b8b12cb400121760d2863
Eric Pinzur <ericpinzur@otherinbox.com>
2017-02-25 15:31:37 -0600

    updated api to be more reasonable

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,18 @@ var express = require('express');
 var router = express.Router();
 var marantz = require('../marantz-client');
 
+var sources = [
+    'AppleTV',
+    'Roku',
+    'ChromeCast'
+]
+
 router.get('/state', function(req, res, next) {
     res.status(200).json(marantz.getState());
 });
 
 router.get('/sources', function(req, res, next) {
-    res.status(200).json([
-        'AppleTV',
-        'Roku',
-        'ChromeCast'
-    ]);
+    res.status(200).json(sources);
 });
 
 router.post('/zone/:zone/volume/up', function(req, res, next) {
@@ -24,13 +26,32 @@ router.post('/zone/:zone/volume/down', function(req, res, next) {
     res.sendStatus(200);
 });
 
-router.post('/zone/:zone/volume/:level', function(req, res, next) {
-    marantz.setVolume(req.params.zone, req.params.level);
-    res.sendStatus(200);
+router.get('/zone/:zone/volume', function(req, res, next) {
+    res.status(200).json({level: marantz.getState().main.mainLevel});
 });
 
-router.post('/zone/:zone/source/:source', function(req, res, next) {
-    if (marantz.setSource(req.params.zone, req.params.source)) {
+router.post('/zone/:zone/volume', function(req, res, next) {
+    var level = parseInt(req.body.level);
+    if (level < 0 || level > 255) {
+        res.status(400).json({'error' : 'level must be between 0 and 255'});
+    }
+    if (marantz.setVolume(req.params.zone, level)) {
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+router.get('/zone/:zone/source', function(req, res, next) {
+    res.status(200).json({source: marantz.getState().main.source});
+});
+
+router.post('/zone/:zone/source', function(req, res, next) {
+    if (sources.indexOf(req.body.source) === -1) {
+        res.status(400).json({'error' : 'source must be one of ' + JSON.stringify(sources)});
+    }
+
+    if (marantz.setSource(req.params.zone, req.body.source)) {
         res.sendStatus(200);
     } else {
         res.sendStatus(404);
